b38f7af0b95615ff1d003c0320c05b3cbc86fe9f
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-11-22 20:08:14 +0530

    Add feature to create dot on touching position in frame

--- ARMeasure/ViewController.swift
+++ ARMeasure/ViewController.swift
@@ -13,20 +13,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    //MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.debugOptions = [.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,30 +40,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    //MARK: - ARSCNViewDelegate methods
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        
+        let touchLocation = touch.location(in: sceneView)
+        
+        let result = sceneView.raycastQuery(from: touchLocation, allowing: .existingPlaneInfinite, alignment: .horizontal)
+        
+        guard let hitLocation = result else {return}
+        
+        createDot(hitLocation.direction.x, hitLocation.direction.y, hitLocation.direction.z)
+    }
+    
+    //MARK: - Dot Methods
+    
+    func createDot(_ x:Float,_ y:Float,_ z:Float){
+        let sphare = SCNSphere(radius: 0.01)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        sphare.materials = [material]
+        
         let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+        node.position = SCNVector3(x, y, z)
+        node.geometry = sphare
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        sceneView.scene.rootNode.addChildNode(node)
     }
 }
